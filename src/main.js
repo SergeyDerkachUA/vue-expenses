6d20063a80886d6890d92ac25231e623cd427474
SergeyDerkachUA <SergeyDerkachKiev@gmail.com>
2020-08-24 12:07:25 +0300

    firebase

--- src/main.js
+++ src/main.js
@@ -1,20 +1,50 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from '../filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
+
+
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAcbGxFK-PFm5ngfrgXdqGbodSX3ii-2n4",
+  authDomain: "vue-expenses-c5dd6.firebaseapp.com",
+  databaseURL: "https://vue-expenses-c5dd6.firebaseio.com",
+  projectId: "vue-expenses-c5dd6",
+  storageBucket: "vue-expenses-c5dd6.appspot.com",
+  messagingSenderId: "966660800517",
+  appId: "1:966660800517:web:4fc3424ece93f052fc5c6c",
+  measurementId: "G-S99VKTSVK3"
+})
+
+let app 
+
+firebase.auth().onAuthStateChanged(() =>{
+
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -58,6 +58,9 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators'
+import messages from '@/utils/messages'
+
+
 export default {
     name: 'login',
     data: () => ({
@@ -69,8 +72,13 @@ export default {
       email: {email, required},
       password: {required, minLength: minLength(8)}
     },
+    mounted() {
+      if(messages[this.$route.query.message]) {
+        this.$message(messages[this.$route.query.message])
+      }
+    },
   methods: {
-    submitHandler() {
+    async  submitHandler() {
       if(this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -80,8 +88,14 @@ export default {
         email: this.email,
         password: this.password
       }
+      try {
+            await this.$store.dispatch('login', formData)
+            this.$router.push('/')
+      } catch(e) {
+        
+      }
+  
       
-      this.$router.push('/')
     }
   }
 }
